648e7a9f06a7d94395305f5b213849d36d5eb0c6
FadhliRajwaa <fadhlirajwaarahmana@gmail.com>
2025-05-20 21:31:26 +0700

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { ThemeProvider } from './context/ThemeContext';
 import App from './App.jsx';
 import './index.css';
 
+// Gunakan HashRouter saat di production untuk menghindari masalah routing pada server static
+// BrowserRouter memerlukan konfigurasi server side untuk menangani routing
+const Router = import.meta.env.PROD ? HashRouter : BrowserRouter;
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <ThemeProvider>
         <App />
       </ThemeProvider>
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 );
