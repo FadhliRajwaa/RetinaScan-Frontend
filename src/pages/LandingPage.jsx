d94566fdeef364de04b207778bbee5155bbc815e
FadhliRajwaa <fadhlirajwaarahmana@gmail.com>
2025-05-25 01:52:57 +0700

    fix

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+// eslint-disable-next-line no-unused-vars
+import { motion } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
 import VantaBackground from '../components/VantaBackground';
 
